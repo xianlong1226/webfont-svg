5d41c297c1144295788fb0128f2b74b86b04c4be
宪龙 <xianlong.zeng@zhaopin.com.cn>
2018-03-02 09:31:17 +0800

    增加刷新功能按钮

--- static/js/test.js
+++ static/js/test.js
@@ -2,4 +2,14 @@ $(function() {
     AjaxGet('/index/getnumber', function(result) {
         $('#font-test-area').html(result.data)
     })
+
+    $('#btn-refresh').click(function(){
+        AjaxGet('/change', function(result) {
+            setTimeout(function(){
+                AjaxGet('/index/getnumber', function(result) {
+                    $('#font-test-area').html(result.data)
+                })
+            }, 1)
+        })
+    })
 })
